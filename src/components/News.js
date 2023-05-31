aaab26fbcfc7e82e85cee6e52b9dda7e09b9083d
Tushar Aggarwal <aggtushar123@gmail.com>
2023-06-01 01:10:10 +0530

    Fetch completed

--- src/components/News.js
+++ src/components/News.js
@@ -1,46 +1,100 @@
-import React, { Component } from 'react'
-import NewsItems from './NewsItems'
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
 
 export class News extends Component {
-//    articles = [{
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page:1
 
-//    }]
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=25589c1335904cea95c57d19ceb241b6&page=1&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults
+    });
+  }
+
+  handleNextClick = async () => {
+    console.log("Next")
+    if(this.state.page+1>(Math.ceil(this.state.totalResults/20))){
+
+    }
+    else{
+
     
-    constructor(){
-        console.log("cons")
-       super();
-       this.state = {
-        articles: [],
-        loading: false 
-       }
-    }
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=25589c1335904cea95c57d19ceb241b6&page=${this.state.page + 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
 
-    async componentDidMount() {
-        let url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=25589c1335904cea95c57d19ceb241b6'
-        let data = await fetch(url)
-        let parsedData = await data.json()
-        
-        this.setState({
-            articles: parsedData.articles
-        }) 
-    }
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page + 1
+    });
 
+    }
+  };
+
+  handlePrevClick = async() => {
+
+    console.log("Prev")
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=25589c1335904cea95c57d19ceb241b6&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1
+    });
+  };
 
   render() {
     return (
-      <div className='container my-3'>
+      <div className="container my-3">
         <h2>NewsMonkey - Top Headlines</h2>
         <div className="row">
-        {this.state.articles.map((element)=>{
-            return <div className="col-md-4" key={element.url}>
-        <NewsItems title={element.title} description = {element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItems
+                  title={element.title}
+                  description={element.description}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
         </div>
-        })}
-    
+
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
